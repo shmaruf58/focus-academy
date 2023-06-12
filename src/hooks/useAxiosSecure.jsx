75f55a883835abc016c2058d12eace39a740daa9
shmaruf58 <maruf414100masum@gmail.com>
2023-06-12 21:17:29 +0600

    use axiosSecure

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,39 +1,36 @@
-import { useEffect } from 'react';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-import useAuth from './useAuth';
-
+import { useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import useAuth from "./useAuth";
 
 const useAxiosSecure = () => {
-  const { logOut } = useAuth(); 
-  const navigate = useNavigate(); 
-
+  const { logOut } = useAuth();
+  const navigate = useNavigate();
   const axiosSecure = axios.create({
-    baseURL: 'https://twin-server.vercel.app', 
+    baseURL: "https://twin-server.vercel.app",
   });
-
   useEffect(() => {
     axiosSecure.interceptors.request.use((config) => {
-      const token = localStorage.getItem('access-token');
+      const token = localStorage.getItem("access-token");
       if (token) {
         config.headers.Authorization = `Bearer ${token}`;
       }
       return config;
     });
-
     axiosSecure.interceptors.response.use(
       (response) => response,
       async (error) => {
-        if (error.response && (error.response.status === 401 || error.response.status === 403)) {
+        if (
+          error.response &&
+          (error.response.status === 401 || error.response.status === 403)
+        ) {
           await logOut();
-          navigate('/login');
+          navigate("/login");
         }
         return Promise.reject(error);
       }
     );
   }, [logOut, navigate, axiosSecure]);
-
   return [axiosSecure];
 };
-
 export default useAxiosSecure;
