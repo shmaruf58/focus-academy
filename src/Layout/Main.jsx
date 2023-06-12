b3b078ff85dc4577676ff2a9c5467e5fa888de7d
shmaruf58 <maruf414100masum@gmail.com>
2023-06-12 21:20:30 +0600

    main jsx

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -3,25 +3,24 @@ import Footer from "../Pages/Shared/Footer/Footer";
 import Navbar from "../Pages/Shared/Navbar/Navbar";
 import { useEffect, useState } from "react";
 
-
 const Main = () => {
-  const [theme, setTheme] = useState('light');
+  const [theme, setTheme] = useState("light");
 
   useEffect(() => {
-    document.documentElement.setAttribute('data-theme', theme);
+    document.documentElement.setAttribute("data-theme", theme);
   }, [theme]);
 
   const toggleTheme = () => {
-    const newTheme = theme === 'light' ? 'night' : 'light';
+    const newTheme = theme === "light" ? "night" : "light";
     setTheme(newTheme);
   };
-    return (
-        <>
-          <Navbar theme={theme} toggleTheme={toggleTheme} ></Navbar>
-          <Outlet></Outlet>  
-          <Footer></Footer>
-        </>
-    );
+  return (
+    <>
+      <Navbar theme={theme} toggleTheme={toggleTheme}></Navbar>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </>
+  );
 };
 
 export default Main;
