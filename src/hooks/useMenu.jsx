0b5013b8450196f9cb9bd30a105de89840bd4129
shmaruf58 <maruf414100masum@gmail.com>
2023-06-12 21:19:10 +0600

    use menu

--- src/hooks/useMenu.jsx
+++ src/hooks/useMenu.jsx
@@ -1,15 +1,16 @@
 import { useQuery } from "@tanstack/react-query";
-
 const useMenu = () => {
-    const {data: menu = [], isLoading: loading, refetch} = useQuery({
-        queryKey: ['menu'],
-        queryFn: async() => {
-            const res = await fetch('https://twin-server.vercel.app/menu');
-            return res.json();
-        }
-    })
-
-    return [menu, loading, refetch]
-}
-
+  const {
+    data: menu = [],
+    isLoading: loading,
+    refetch,
+  } = useQuery({
+    queryKey: ["menu"],
+    queryFn: async () => {
+      const res = await fetch("https://twin-server.vercel.app/menu");
+      return res.json();
+    },
+  });
+  return [menu, loading, refetch];
+};
 export default useMenu;
