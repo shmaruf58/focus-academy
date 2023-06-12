0206bd5ed06eec514a88c2c9b0981fb6e506964e
shmaruf58 <maruf414100masum@gmail.com>
2023-06-12 21:23:31 +0600

    instructor home

--- src/Pages/Dashboard/InstaHome/InstaHome.jsx
+++ src/Pages/Dashboard/InstaHome/InstaHome.jsx
@@ -1,15 +1,16 @@
 import { Helmet } from "react-helmet-async";
 
-
 const InstaHome = () => {
-    return (
-        <div>
-            <Helmet>
+  return (
+    <div>
+      <Helmet>
         <title>Focus Academy | Instructor Home</title>
       </Helmet>
-               <h1 className="text-center text-5xl font-bold">Welcome To Instructor Home</h1>
-        </div>
-    );
+      <h1 className="text-center text-5xl font-bold">
+        Welcome To Instructor Home
+      </h1>
+    </div>
+  );
 };
 
 export default InstaHome;
